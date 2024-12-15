9ec76cfa00333f7780ea56cfbc2d9b671cb8c3ef
mrtxni <akunikshannn@gmail.com>
2024-12-15 17:41:08 +0700

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,29 @@
+#(©)Codeflix-Bots
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>Kucing</a>\n○ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/kucingemas_v'>information</a><a href='https://www.youtube.com/'>Repo</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚡️ ᴄʟᴏsᴇ", callback_data = "close"),
+                    InlineKeyboardButton('🍁 Join vip', url='https://t.me/cumasiska')
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
